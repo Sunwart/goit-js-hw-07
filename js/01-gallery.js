f8bebcfd731cef7cd11b59be1f936278f7863403
Sunwart <li.sa.vectorart@gmail.com>
2021-10-23 04:04:45 +0300

    Task 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.innerHTML = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<div class="gallery__item"><a href="${original}" class="gallery__link"><img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"></a></div>`,
+  )
+  .join('');
+
+galleryElem.addEventListener('click', viewImage);
+
+function viewImage(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  event.preventDefault();
+  const imageInstance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+
+  imageInstance.show();
+
+  let visible = true;
+
+  document.addEventListener('keydown', closeModal);
+
+  function closeModal(event) {
+    if (event.key === 'Escape') {
+      imageInstance.close();
+      document.removeEventListener('keydown', closeModal);
+      visible = false;
+    }
+
+    stopListeningKeyboard();
+  }
+
+  function stopListeningKeyboard() {
+    if (visible !== imageInstance.visible()) {
+      document.removeEventListener('keydown', closeModal);
+    }
+  }
+}
