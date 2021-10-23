5857032e6720b6b9cb9188e77c908fc8bb0303cd
Sunwart <li.sa.vectorart@gmail.com>
2021-10-23 04:52:15 +0300

    Task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.innerHTML = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a href="${original}" class="gallery__item"><img src="${preview}" alt="${description}" title="${description}" class="gallery__image"></a>`,
+  )
+  .join('');
+
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
